8de82e9c0b15d4dbafd64dde14b9d872b3c935a8
ruohan <1534007973@qq.com>
2019-07-26 14:50:28 +0800

    fix error

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,12 +1,69 @@
 module.exports = [
   {
-    path: '/peoduct',
     name: '产品管理',
-    icon: 'edit',
+    icon: 'tag',
+    items: [
+      {
+        path: '/product/categroy',
+        name: '产品类别',
+        icon: 'tags',
+      },
+      {
+        path: '/product',
+        name: '产品',
+        icon: 'tags',
+      },
+      {
+        path: '/product_tag',
+        name: '产品标签',
+        icon: 'tags',
+      },
+      {
+        path: '/trial',
+        name: '试用装管理',
+        icon: 'tags',
+      },
+      {
+        path: '/productsStock',
+        name: '评价管理',
+        icon: 'tags',
+      },
+      {
+        path: '/fare_template',
+        name: '运费管理',
+        icon: 'tags',
+      },
+    ]
   },
   {
-    path: '/oder',
     name: '订单管理',
-    icon: 'scissor',
+    icon: 'tag',
+    items:[
+      {
+        path: '/order',
+        name: '线上订单',
+        icon: 'tag',
+      },
+      {
+        path: '/store_order',
+        name: '线下订单',
+        icon: 'tag',
+      },
+      {
+        path: '/return_refund_order',
+        name: '退货处理',
+        icon: 'tag',
+      },
+      {
+        path: '/trial_application',
+        name: '试用装申请',
+        icon: 'tag',
+      },
+      {
+        path: '/express',
+        name: '快递设置',
+        icon: 'tag',
+      }
+    ]
   },
 ]
---
+++ src/pages/product/categroy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Category = ({ }) => {
+
+  return (
+    <div>
+      aaaaaaaa
+    </div>
+  )
+}
+
+export default Category
